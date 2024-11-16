65f6c96f18a67f790357e6529836176ff3bcb713
Brucezhuu <2214385429@qq.com>
2024-11-16 16:37:09 +0800

    stock query and grpc

--- internal/payment/go.mod
+++ internal/payment/go.mod
@@ -2,6 +2,4 @@ module github.com/Brucezhuu/goorder/internal/payment
 
 go 1.22.8
 
-replace (
-	github.com/Brucezhuu/goorder/internal/payment => ../payment
-)
+replace github.com/Brucezhuu/goorder/internal/payment => ../payment
--- internal/stock/service/application.go
+++ internal/stock/service/application.go
@@ -2,9 +2,22 @@ package service
 
 import (
 	"context"
+	"github.com/Brucezhuu/goorder/internal/common/metrics"
+	"github.com/Brucezhuu/goorder/internal/stock/adapters"
 	"github.com/Brucezhuu/goorder/internal/stock/app"
+	"github.com/Brucezhuu/goorder/internal/stock/app/query"
+	"github.com/sirupsen/logrus"
 )
 
-func NewApplication(ctx context.Context) app.Application {
-	return app.Application{}
+func NewApplication(_ context.Context) app.Application {
+	stockRepo := adapters.NewMemoryStockRepository()
+	logger := logrus.NewEntry(logrus.StandardLogger())
+	metricsClient := metrics.TodoMetrics{}
+	return app.Application{
+		Commands: app.Commands{},
+		Queries: app.Queries{
+			CheckIfItemsInStock: query.NewCheckIfItemsInStockHandler(stockRepo, logger, metricsClient),
+			GetItems:            query.NewGetItemsHandler(stockRepo, logger, metricsClient),
+		},
+	}
 }
