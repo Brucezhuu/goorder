ebc88c9a2b17f39a4a8af3f1999358569cd7177d
Brucezhuu <2214385429@qq.com>
2024-11-24 23:34:31 +0800

    errno

---
+++ internal/common/consts/errno.go
@@ -0,0 +1,15 @@
+package consts
+
+const (
+	ErrnoSuccess               = 0
+	ErrnoUnknownError          = 1
+	ErrnoBindRequestsError     = 1000
+	ErrnoRequestsValidateError = 1001
+)
+
+var ErrMsg = map[int]string{
+	ErrnoSuccess:               "success",
+	ErrnoUnknownError:          "unknown error",
+	ErrnoBindRequestsError:     "binding requests failed",
+	ErrnoRequestsValidateError: "requests validation failed",
+}
